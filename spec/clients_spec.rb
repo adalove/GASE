a7bd22b06fd00d99f457bd1e753b946fb89abbf4
NickyH <nickyhughes00@gmail.com>
2013-02-02 08:54:50 -0500

    Attmepting to merge back to master

--- spec/clients_spec.rb
+++ spec/clients_spec.rb
@@ -6,7 +6,7 @@ let(:client) {Client.new("Bob")} #this allows same code below to be removed so l
 
   describe ".new" do
     it "creates a Client object" do #'it' is the first test
-      expect(clint).to_not eq nil
+      expect(client).to_not eq nil
     end
     # it "has no accounts" do
     #   expect(bank.accounts.count).to eq 0
